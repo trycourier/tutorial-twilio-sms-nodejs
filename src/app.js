adb45fc3917100fdf83c70ba642b4f039c3cf1ab
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-06-28 03:07:02 +0100

    refactor: revamp the endpoints to fix wrong code and discrepancies

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require("express");
 const { CourierClient } = require("@trycourier/courier");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-const courier = CourierClient({ authorizationToken: process.env.COURIER_AUTH_TOKEN });
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Bookstore App!');
+const courier = CourierClient({
+  authorizationToken: process.env.COURIER_AUTH_TOKEN,
 });
 
-app.post('/orders', async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Welcome to the Bookstore App!");
+});
+
+app.post("/orders", async (req, res) => {
   const { customerName, customerPhoneNumber, orderNumber } = req.body;
 
   if (!customerName || !customerPhoneNumber || !orderNumber) {
-    return res.status(400).json({ error: 'Missing required field' });
+    return res.status(400).json({ error: "Missing required field" });
   }
 
   try {
@@ -24,29 +25,49 @@ app.post('/orders', async (req, res) => {
     console.log(`Creating order ${orderNumber} for ${customerName}`);
 
     // send order confirmation SMS
-    const messageId = await sendOrderConfirmationSMS(customerName, customerPhoneNumber, orderNumber);
+    const requestId = await sendOrderConfirmationSMS(
+      customerName,
+      customerPhoneNumber,
+      orderNumber
+    );
 
-    res.status(200).json({ message: 'Order created successfully', messageId });
+    res
+      .status(200)
+      .json({ message: "Order created successfully", request_id: requestId });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to create order' });
+    res.status(500).json({ error: "Failed to create order" });
   }
 });
 
-async function sendOrderConfirmationSMS(customerName, customerPhoneNumber, orderNumber) {
-  const { messageId } = await courier.send({
-    eventId: "order_confirmation",
-    recipientId: customerPhoneNumber,
-    profile: {
-      phone_number: customerPhoneNumber
+async function sendOrderConfirmationSMS(
+  customerName,
+  customerPhoneNumber,
+  orderNumber
+) {
+  const { requestId } = await courier.send({
+    message: {
+      to: {
+        phone_number: customerPhoneNumber,
+        data: {
+          order_id: orderNumber,
+          customer_name: customerName,
+        },
+      },
+      content: {
+        title: "Confirm your order",
+        body: `Hello {{customer_name}}, 
+        We have placed your order, {{order_id}}
+        `,
+      },
+      routing: {
+        method: "single",
+        channels: ["sms"],
+      },
     },
-    data: {
-      customerName,
-      orderNumber
-    }
   });
 
-  return messageId;
+  return requestId;
 }
 
 app.post("/send-sms-multiple", async (req, res) => {
@@ -54,31 +75,50 @@ app.post("/send-sms-multiple", async (req, res) => {
   const toRecipients = [];
 
   for (const recipient of recipients) {
-    await courier.profiles.replace(recipient.id, { phone_number: recipient.phoneNumber });
-    toRecipients.push({ user_id: recipient.id, phone_number: recipient.phoneNumber, data: { name: recipient.name } });
+    try {
+      let d = await courier.replaceProfile({
+        recipientId: recipient.id,
+        profile: {
+          phone_number: recipient.phoneNumber,
+        },
+      });  
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to create/update profile" });
+    }
+
+    toRecipients.push({
+      user_id: recipient.id,
+      phone_number: recipient.phoneNumber,
+      data: {
+        name: recipient.name,
+        tracking_number: recipient.trackingNumber,
+        courier: recipient.courier,
+      },
+    });
   }
 
-  const { requestId } = await courier.send({
-    message: {
-      to: toRecipients,
-      content: {
-        title: "Delivery Updates",
-        body: "Hello {{name}}, your package with tracking number {{tracking_number}} has been dispatched by {{courier}}.",
+  try {
+    const { requestId } = await courier.send({
+      message: {
+        to: toRecipients,
+        content: {
+          title: "Delivery Updates",
+          body: "Hello {{name}}, your package with tracking number {{tracking_number}} has been dispatched by {{courier}}.",
+        },
+        routing: {
+          method: "all",
+          channels: ["sms"],
+        },
       },
-      routing: {
-        method: "all",
-        channels: ["sms"],
-      },
-    },
-    data: {
-      tracking_number: "1234567890",
-      courier: "Fast Delivery",
-    },
-  });
-
-  res.json({ messageId: requestId });
+    });
+  
+    res.json({ request_id: requestId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to send SMS" });
+  }
 });
-
 
 const PORT = process.env.PORT || 3000;
 
